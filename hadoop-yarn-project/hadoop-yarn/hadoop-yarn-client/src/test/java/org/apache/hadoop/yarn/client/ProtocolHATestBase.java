d61245a8e1e271fa67641f9fab2286c121e192b4
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2016-10-27 18:51:24 +0200

    Fixing HA failing tests

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/ProtocolHATestBase.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-client/src/test/java/org/apache/hadoop/yarn/client/ProtocolHATestBase.java
@@ -22,9 +22,6 @@ import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.hops.util.DBUtility;
-import io.hops.util.RMStorageFactory;
-import io.hops.util.YarnAPIStorageFactory;
 import org.apache.hadoop.yarn.api.protocolrecords.FinishApplicationMasterRequest;
 import org.apache.hadoop.yarn.api.protocolrecords.FinishApplicationMasterResponse;
 import org.apache.hadoop.yarn.api.protocolrecords.RegisterApplicationMasterRequest;
@@ -165,6 +162,29 @@ public abstract class ProtocolHATestBase extends ClientBaseWithFixes {
   protected Thread failoverThread = null;
   private volatile boolean keepRunning;
 
+  private void setConfForRM(String rmId, String prefix, String value) {
+    conf.set(HAUtil.addSuffix(prefix, rmId), value);
+  }
+
+  private void setRpcAddressForRM(String rmId, int base) {
+    String hostname = "localhost";
+    setConfForRM(rmId, YarnConfiguration.RM_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_SCHEDULER_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_SCHEDULER_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_ADMIN_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_ADMIN_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_RESOURCE_TRACKER_ADDRESS,
+        hostname + ":" + (base + YarnConfiguration
+            .DEFAULT_RM_RESOURCE_TRACKER_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_WEBAPP_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_WEBAPP_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_WEBAPP_HTTPS_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_WEBAPP_HTTPS_PORT));
+    setConfForRM(rmId, YarnConfiguration.RM_GROUP_MEMBERSHIP_ADDRESS, hostname + ":" +
+        (base + YarnConfiguration.DEFAULT_RM_GROUP_MEMBERSHIP_PORT));
+  }
+
   @Before
   public void setup() throws IOException {
     failoverThread = null;
@@ -180,9 +200,6 @@ public abstract class ProtocolHATestBase extends ClientBaseWithFixes {
 
     conf.setBoolean(YarnConfiguration.YARN_MINICLUSTER_FIXED_PORTS, true);
     conf.setBoolean(YarnConfiguration.YARN_MINICLUSTER_USE_RPC, true);
-    RMStorageFactory.setConfiguration(conf);
-    YarnAPIStorageFactory.setConfiguration(conf);
-    DBUtility.InitializeDB();
   }
 
   @After
@@ -275,14 +292,21 @@ public abstract class ProtocolHATestBase extends ClientBaseWithFixes {
   }
 
   protected void startHACluster(int numOfNMs, boolean overrideClientRMService,
-      boolean overrideRTS, boolean overrideApplicationMasterService)
+          boolean overrideRTS, boolean overrideApplicationMasterService)
+          throws Exception {
+    startHACluster(numOfNMs, overrideClientRMService, overrideRTS,
+            overrideApplicationMasterService, true);
+  }
+
+  protected void startHACluster(int numOfNMs, boolean overrideClientRMService,
+      boolean overrideRTS, boolean overrideApplicationMasterService, boolean formatDB)
       throws Exception {
     conf.setBoolean(YarnConfiguration.RECOVERY_ENABLED, true);
     conf.setBoolean(YarnConfiguration.AUTO_FAILOVER_ENABLED, false);
     cluster =
         new MiniYARNClusterForHATesting(TestRMFailover.class.getName(), 2,
             numOfNMs, 1, 1, false, overrideClientRMService, overrideRTS,
-            overrideApplicationMasterService);
+            overrideApplicationMasterService, formatDB);
     cluster.resetStartFailoverFlag(false);
     cluster.init(conf);
     cluster.start();
@@ -310,11 +334,20 @@ public abstract class ProtocolHATestBase extends ClientBaseWithFixes {
     private final AtomicBoolean failoverTriggered = new AtomicBoolean(false);
 
     public MiniYARNClusterForHATesting(String testName,
+            int numResourceManagers, int numNodeManagers, int numLocalDirs,
+            int numLogDirs, boolean enableAHS, boolean overrideClientRMService,
+            boolean overrideRTS, boolean overrideApplicationMasterService) {
+      this(testName, numResourceManagers, numNodeManagers, numLocalDirs,
+              numLogDirs, enableAHS, overrideClientRMService, overrideRTS,
+              overrideApplicationMasterService, true);
+    }
+
+    public MiniYARNClusterForHATesting(String testName,
         int numResourceManagers, int numNodeManagers, int numLocalDirs,
         int numLogDirs, boolean enableAHS, boolean overrideClientRMService,
-        boolean overrideRTS, boolean overrideApplicationMasterService) {
+        boolean overrideRTS, boolean overrideApplicationMasterService, boolean formatDB) {
       super(testName, numResourceManagers, numNodeManagers, numLocalDirs,
-          numLogDirs, enableAHS);
+          numLogDirs, enableAHS, formatDB);
       this.overrideClientRMService = overrideClientRMService;
       this.overrideRTS = overrideRTS;
       this.overrideApplicationMasterService = overrideApplicationMasterService;
